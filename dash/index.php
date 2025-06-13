c5d55f63adde5b600743bcdfdf6bec410cb74a4c
Cristian Ceballos <ceballosmarincristiancamilo@gmail.com>
2025-06-13 05:39:09 -0500

    se finaliza todo el proyecto

--- dash/index.php
+++ dash/index.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <div class="card mb-0">
                             <div class="card-body">
                                 <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
-                                    <img src="../img/logo-blanco.png" width="180" alt="">
+                                    <img src="../img/logo-nix.png" width="180" alt="">
                                 </a>
                                 <p class="text-center">&copy; Nixcol </p>
 
@@ -99,7 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                             </label>
                                         </div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4 rounded-2">Vamos</button>
+                                    <button type="submit" class="btn btn-dark w-100 py-8 fs-4 mb-4 rounded-2">Vamos</button>
                                     <div class="d-flex align-items-center justify-content-center">
                                     </div>
                                 </form>
